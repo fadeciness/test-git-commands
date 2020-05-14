26151afd83b61418be90a882b3cdd19da571c0fc
Evgeny <55512938+fadeciness@users.noreply.github.com>
2020-05-14 22:50:04 +0300

    feature_my_1 (#1)
    squash commit into feature_my_1 branch

--- src/main/java/FirstFile.java
+++ src/main/java/FirstFile.java
@@ -1,7 +1,10 @@
+import java.util.Random;
+
 public class FirstFile {
 
     public static void main(String[] args) {
+        Random random = new Random();
+        System.out.println(random.doubles());
         System.out.println("It's some fix");
     }
-
 }
