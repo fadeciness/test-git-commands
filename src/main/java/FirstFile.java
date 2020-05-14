63f863986abce36b715625dcaa6c0d271c989f89
Eugene Solodimov <solodimov.eugene@yandex.ru>
2020-05-14 22:30:18 +0300

    feature_my_1 5. 1 files created

---
+++ src/main/java/FirstFile.java
@@ -0,0 +1,9 @@
+import java.util.Random;
+
+public class FirstFile {
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        System.out.println(random.doubles());
+    }
+}
